544abad95130a4d7a73eb291638bfa09818d204f
lyt251 <69009242+lyt251@users.noreply.github.com>
2020-10-02 23:55:15 -0700

    adding nav and another controller

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Tina Ly on 10/2/20.
+//  Copyright © 2020 Tina Ly. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
